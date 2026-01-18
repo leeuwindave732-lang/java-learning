12a62fe26089b6beb4aa7e4165b1201d20d4ba25
leeuwindave732-lang <leeuwindave732@gmail.com>
2026-01-18 13:05:44 +0800

    Day 9

---
+++ Day9/Strings1.java
@@ -0,0 +1,28 @@
+public class Strings1 {
+    public static void main(String[] args) {
+        // String
+        String name = "Java";
+
+        System.out.println(name.length()); // 4
+        System.out.println(name.toUpperCase()); // JAVA
+        System.err.println(name.toLowerCase()); // java
+        System.out.println(name.substring(0, 2)); // ja
+
+        // String Comparison
+        String a = "test";
+        String b = "test";
+        System.out.println(a.equals(b)); // true
+
+
+        // StringBuilder
+
+        StringBuilder sb = new StringBuilder("Hello");
+
+        sb.append("World"); // Add new string
+        sb.insert(5, " "); // Add Space
+        sb.replace(0, 5, "Hi"); // "Hello" -> "Hi"
+
+        System.out.println(sb.toString()); // Hi World
+
+    }
+}
---
+++ Day9/miniProject9.java
@@ -0,0 +1,38 @@
+public class miniProject9 {
+    public static void main(String[] args) {
+        // Reversed a string using StringBuilder
+
+        String word = "Seiju Hajime";
+        StringBuilder rev = new StringBuilder();
+
+        rev.append(word);
+
+        rev.reverse();
+
+        System.out.println(rev);
+
+        // Count Vowels in a string
+        int count = 0;
+        String sentence = "Hi! My name is Leeuwin, I'm a Computer Science Student";
+
+        for ( int i = 0; i < sentence.length(); i++ ) {
+            char ch = sentence.charAt(i);
+            switch (ch) {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                case 'A': case 'E': case 'I': case 'O': case 'U':
+                    count++;
+                    break;
+            }
+        }
+        System.out.println("Number of vowels in a a sentence:" + count );
+
+        // Build a sentence by appending words in a loop
+        String[] str = { "Hi, ", "My name ", "is Seiju" };
+        StringBuilder strSentence = new StringBuilder();
+        for ( String sen: str) {
+            strSentence.append(sen);
+        }
+
+        System.out.println(strSentence);
+    }
+}
